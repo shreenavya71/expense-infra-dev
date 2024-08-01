cc7392e4f4adf37df2accbd53b826e53d3b5c404
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-08-01 23:34:42 +0100

    new

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -155,3 +155,11 @@ resource "aws_security_group_rule" "bastion_public" {
     security_group_id = module.bastion.sg_id
 }
 
+resource "aws_security_group_rule" "app_alb_vpn" {
+    type              = "ingress"
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    source_security_group_id = module.vpn.sg_id
+    security_group_id = module.app_alb.sg_id
+}
