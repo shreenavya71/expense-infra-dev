ba32b0384dad19ae811bc6500f508e6b7caa10e3
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-08-04 18:34:57 +0100

    new

--- 07-backend/main.tf
+++ 07-backend/main.tf
@@ -83,3 +83,60 @@ resource "aws_lb_target_group" "backend" {
         matcher             = "200"
     }
 }
+
+resource "aws_launch_template" "backend" {
+    name = "${var.project_name}-${var.environment}-${var.common_tags.component}"
+    image_id = "aws_ami_instance_backend.id"
+    instance_initiated_shutdown_behavior = "terminate"
+    instance_type = "t3.micro"
+    update_default_version = true # sets the latest version to default version
+    vpc_security_group_ids = ["data.aws_ssm_parameter.backend_sg_id.value"]
+    tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+        var.common_tags,
+        {
+            Name = "${var.project_name}-${var.environment}-${var.common_tags.component}"
+        }
+    )
+}
+}
+
+resource "aws_autoscaling_group" "backend" {
+    name                      = "${var.project_name}-${var.environment}-${var.common_tags.component}"
+    max_size                  = 5
+    min_size                  = 1
+    health_check_grace_period = 60
+    health_check_type         = "ELB"
+    desired_capacity          = 1
+    launch_template {
+        id      = aws_launch_template.backend.id
+        version = "$Latest"
+    }
+    vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+
+    instance_refresh {
+    strategy = "Rolling"
+    preferences {
+        min_healthy_percentage = 50
+    }
+        triggers = ["launch_template"]
+    }
+    tag {
+    key                 = "Name"
+    value               = "${var.project_name}-${var.environment}-${var.common_tags.component}"
+    propagate_at_launch = true
+    }
+
+    timeouts {
+        delete = "15m"
+    }
+
+    tag {
+        key                 = "project"
+        value               = "$(var.project_name)"
+        propagate_at_launch = false
+    }
+}
+
