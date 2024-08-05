f1eca6e14ebb91126ec148627e628b697266839b
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-08-06 00:25:18 +0100

    new

--- 08-web-alb/parameters.tf
+++ 08-web-alb/parameters.tf
@@ -4,7 +4,7 @@ resource "aws_ssm_parameter" "web_alb_listener_arn" {
     value = aws_lb_listener.http.arn
 }
 
-resource "aws_ssm_parameter" "web_alb_listener_arn" {
+resource "aws_ssm_parameter" "web_alb_listener_arn_https" {
     name  = "/${var.project_name}/${var.environment}/web_alb_listener_arn_https"
     type  = "String"
     value = aws_lb_listener.https.arn
